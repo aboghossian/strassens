9906ccd8aa7fc29af5a1bc2543abcff936670d23
aboghossian <aboghoss@broadinstitute.org>
2020-03-24 10:46:39 -0400

    simple matrix operations

--- matrix_operations.py
+++ matrix_operations.py
@@ -1,8 +1,19 @@
 def add_matrices(a, b):
-    return
+    n = len(a)
+    out_mat = [[a[i][j] + b[i][j] for j in range(n)] for i in range(n)]
+    return out_mat
 
 def subtract_matrices(a, b):
-    return
+    n = len(a)
+    out_mat = [[a[i][j] - b[i][j] for j in range(n)] for i in range(n)]
+    return out_mat
 
 def multiply_matrices(a, b):
-    return
+    out_mat = [[0 for i in range(len(b[0]))] for j in range(len(a))]
+
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                out_mat[i][j] += (a[i][k] * (b[k][j])
+
+    return out_mat
