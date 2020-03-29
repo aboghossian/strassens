2b06c061c49112c7704f2989628f4a0f93da7aab
aboghossian <aboghoss@broadinstitute.org>
2020-03-29 18:42:13 -0400

    typo fix and padding

--- strassens.py
+++ strassens.py
@@ -1,5 +1,25 @@
-from matrix_operations import *
 from math import ceil, floor, log2
+
+
+def add_matrices(a, b):
+    n = len(a)
+    out_mat = [[a[i][j] + b[i][j] for j in range(n)] for i in range(n)]
+    return out_mat
+
+
+def subtract_matrices(a, b):
+    n = len(a)
+    out_mat = [[a[i][j] - b[i][j] for j in range(n)] for i in range(n)]
+    return out_mat
+
+
+def multiply_matrices(a, b):
+    out_mat = [[0 for i in range(len(b[0]))] for j in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                out_mat[i][j] += a[i][k] * b[k][j]
+    return out_mat
 
 
 def strassens(a, b, crossover=2):
@@ -60,8 +80,3 @@ def strassens(a, b, crossover=2):
         result = top + bottom
 
     return result
-
-m1 = [[1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1]]
-m2 = [[2, 1, 2, 1, 2], [2, 1, 2, 1, 2], [2, 1, 2, 1, 2], [2, 1, 2, 1, 2], [2, 1, 2, 1, 2]]
-
-print(strassens(m1, m2))
