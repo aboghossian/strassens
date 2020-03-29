edda3c455ccf2ff7e796b922fc6b7f8d34a391e2
aboghossian <aboghoss@broadinstitute.org>
2020-03-29 13:26:53 -0400

    minor update

--- strassens.py
+++ strassens.py
@@ -1,2 +1,12 @@
-def strassens(a, b):
-    return
+from matrix_operations import *
+
+
+def strassens(a, b, crossover=2):
+    # base case
+    if length(a) == crossover:
+        result = multiply_matrices(a, b)
+    # recursive case
+    else:
+        # we need to write this part
+        break
+    return result
